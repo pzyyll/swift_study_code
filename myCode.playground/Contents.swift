1c90fbfe1ca447f9f24d80720b1ac4e1c191d54b
pzyyll <pzyyll@gmail.com>
2015-10-08 19:15:53 +0800

    20151008 19:15

---
+++ myCode.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+srandom(UInt32(time(nil)))
+for i in 1..<10 {
+    print(random() % 150 - 50)
+}
+
+var num = 1.2
+var sres = String(format: "%.2f %.2f", arguments: [num, 1.2])
+
+num = 3 *
+ 3
+
+repeat {
+    print(num--)
+}while (num > 0)
+
+5 % 15
