2ae7990abf7dba9135da367413ecc011990fcda4
pzyyll <pzyyll@gmail.com>
2015-10-09 23:40:07 +0800

    20151009 23:39
    还有密码验证次数未完成

--- inCourse/TestIOS/TestIOS/ViewController.swift
+++ inCourse/TestIOS/TestIOS/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let textMail = UITextField(frame: CGRectMake(126, 460, 200, 30))
+    let textUserName = UITextField(frame: CGRectMake(126, 500, 200, 30))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.
+
+        textMail.borderStyle = UITextBorderStyle.RoundedRect
+        textMail.placeholder = "email"
+        self.view.addSubview(textMail)
+        
+        textUserName.borderStyle = UITextBorderStyle.RoundedRect
+        textUserName.placeholder = "username"
+        self.view.addSubview(textUserName)
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,14 +41,14 @@ class ViewController: UIViewController {
     
     @IBAction func OnTouchBnLogin(sender: UIButton) {
         
-        if userName.text.isEmpty {
-            displayUP.text = "请输入密码！！"
+        if userName.text!.isEmpty {
+            displayUP.text! = "请输入密码！！"
             return
         }
 
-        var user = (name: "admin", pwd: "123")
-        if userName.text == user.name &&
-            pwd.text == user.pwd {
+        let user = (name: "admin", pwd: "123")
+        if userName.text! == user.name &&
+            pwd.text! == user.pwd {
                 displayUP.text = "right"
         } else {
             displayUP.text = "error"
@@ -45,6 +56,88 @@ class ViewController: UIViewController {
         
         //print("name \(userName!) pwd \(pwd)")
     }
+    
+    
+    func verifyIsEmail(email: String) -> Bool {
+        var cntAnd = 0, cntDou = 0
+        for c in email.characters {
+            switch c {
+                case "@":
+                    ++cntAnd;
+                case ".":
+                    ++cntDou;
+                default:
+                    break
+            }
+        }
+        if cntAnd != 0 && cntDou != 0 {
+            return true
+        }
+        return false
+    }
+    
+    func pwdIsLegal(pwd: String) -> Bool {
+        if pwd.characters.count <= 6 ||
+            pwd.characters.count > 16 {
+                return false
+        }
+        return true
+    }
+    
+    func verifyUsernameIsLegal(username: String) -> Bool {
+        var upChar = false, lowChar = false, numChar = false
+        
+        for c in username.characters {
+            switch c {
+                case "A"..."Z":
+                    upChar = true;
+                case "a"..."z":
+                    lowChar = true;
+                case "0"..."9":
+                    numChar = true;
+            default:
+                break
+            }
+        }
+        if upChar && lowChar && numChar {
+            return true
+        }
+        return false
+    }
+    
+    @IBAction func OnTouchBnReg(sender: UIButton) {
+//        if !verifyIsEmail(textMail.text!) {
+//            displayUP.text = "邮箱非法！！"
+//            return
+//        }
+//        if !pwdIsLegal(pwd.text!) {
+//            displayUP.text = "密码长度在6~16之间哦"
+//            return
+//        }
+//        
+//        displayUP.text = "注册成功"
+        
+        //用户名注册
+        if textUserName.text!.isEmpty {
+            displayUP.text = "用户名没有填写"
+            return
+        }
+        if !verifyUsernameIsLegal(textUserName.text!) {
+            displayUP.text = "用户名要包含大写字母和小写字母，及数字"
+            return
+        }
+        if pwd.text!.isEmpty {
+            displayUP.text = "密码没有填"
+            return
+        }
+        if !pwdIsLegal(pwd.text!) {
+            displayUP.text = "密码长度在6~16之间哦"
+            return
+        }
+        displayUP.text = "注册成功"
+        
+    }
+    
 }
 
 
