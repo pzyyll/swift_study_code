8f576a286e4d10813936726cc9e587d8ad94762c
pzyyll <pzyyll@gmail.com>
2015-12-18 16:39:56 +0800

    on course
    on course

--- inCourse/musplay/musplay/MusicTableViewController.swift
+++ inCourse/musplay/musplay/MusicTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MusicTableViewController: UITableViewController {
 
@@ -16,6 +17,9 @@ class MusicTableViewController: UITableViewController {
     var btnStop: UIButton? //stop
     
     var sonLibary = [Son]()
+    var songBundle = [String]()
+    var player: AVAudioPlayer!
+    var timeSlider: NSTimer!    //time trigger
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +27,50 @@ class MusicTableViewController: UITableViewController {
         self.btnPause = UIButton()
         self.btnStop = UIButton()
     
+        self.navigationController?.navigationBarHidden = true
         var bgView = UIView(frame: CGRectMake(0, 20, self.view.frame.width, self.view.frame.height / 2))
         self.imgView.frame = bgView.frame
         self.imgView.frame.size.height -= 50
-        self.slider = UISlider(frame: CGRectMake(0, self.imgView, self.view.frame.width, self.view.frame.height / 2))
+        bgView.addSubview(self.imgView)
+        
+        var tool = UIView(frame: CGRectMake(0, imgView.frame.height, self.view.frame.width, 50))
+        tool.backgroundColor = UIColor.blackColor()
+        bgView.addSubview(tool)
+        
+        
+        self.slider = UISlider(frame: CGRectMake(10, 0, tool.frame.width * 2/3, 50))
+        self.slider?.thumbTintColor = UIColor(red: 0.2, green: 0.8, blue: 0.5, alpha: 1.0)
+        self.slider?.tintColor = UIColor(red: 0.2, green: 0.8, blue: 0.5, alpha: 1.0)
+        
+        self.btnPause?.frame = CGRectMake(self.slider!.frame.width + 20, 10, tool.frame.height * 2/3, tool.frame.height * 2/3)
+        self.btnPause?.setBackgroundImage(UIImage(named: "pause.png"), forState: .Normal)
+        self.btnPause?.setTitle("暂停", forState: .Normal)
+        self.btnPause?.setTitleColor(UIColor.clearColor(), forState: .Normal)
+        
+        self.btnStop?.frame = CGRectMake(tool.frame.width - tool.frame.height, 10, tool.frame.height * 2/3, tool.frame.height * 2/3)
+        
+        self.btnStop?.setBackgroundImage(UIImage(named: "stop.png"), forState: .Normal)
+        
+        tool.addSubview(self.slider!)
+        tool.addSubview(self.btnStop!)
+        tool.addSubview(self.btnPause!)
+        
+        self.btnPause?.enabled = false
+        self.btnPause?.enabled = false
+        
+        //butt
+        self.btnPause?.addTarget(self, action: Selector("pause"), forControlEvents: UIControlEvents.TouchUpInside)
+        self.btnStop?.addTarget(self, action: Selector("stop"), forControlEvents: UIControlEvents.TouchUpInside)
+        self.slider?.addTarget(self, action: Selector("progressCtr"), forControlEvents: .ValueChanged)
+        
+        self.tableView.tableHeaderView = bgView
+        self.tableView.backgroundView = UIImageView(image: UIImage(named: "bg.jpg"))
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
+        
+        //4.获取歌曲路径
+        getMp3Paths()
+        
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -45,24 +89,120 @@ class MusicTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return sonLibary.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
+//        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        
+        let cell = UITableViewCell(style: .Value1, reuseIdentifier: "reuseIdentifier")
         // Configure the cell...
-
+        cell.textLabel?.text = sonLibary[indexPath.row].title
+        cell.detailTextLabel?.text = sonLibary[indexPath.row].artist
+        cell.backgroundColor = UIColor.clearColor()
+        
         return cell
     }
-    */
 
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.imgView.image = self.sonLibary[indexPath.row].img
+        
+        //1.create player
+        let mp3Url = NSURL(fileURLWithPath: self.songBundle[indexPath.row])
+        do {
+            self.player = try AVAudioPlayer(contentsOfURL: mp3Url)
+            
+            //播放
+            self.player.prepareToPlay()
+            self.player.numberOfLoops = -1  //set loop times, default all the time loop
+            
+            player.play()
+            self.btnPause?.enabled = true
+            self.btnPause?.enabled = true
+            
+            //every second auto call
+            self.timeSlider = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("timeUp"), userInfo: nil, repeats: true)
+        } catch {
+            print("shit")
+        }
+        
+    }
+    
+    func timeUp() {
+        self.slider?.value = Float(self.player.currentTime / self.player.duration)
+    }
+    
+    func getMp3Paths() {
+        //获取MP3路径
+        let bundle = NSBundle.mainBundle().bundlePath
+        self.songBundle = NSBundle.pathsForResourcesOfType("mp3", inDirectory: bundle)
+        
+        for mp3Path in songBundle {
+            let fileManager = NSFileManager()   //
+            if fileManager.fileExistsAtPath(mp3Path) {
+                let mp3url = NSURL(fileURLWithPath: mp3Path)
+                print(mp3url)
+                print(sonLibary.count)
+                //2.获取信息
+                let mp3Asset = AVURLAsset(URL: mp3url, options: nil)
+                let song = Son()
+                for format in mp3Asset.availableMetadataFormats {
+                    for metadataItem in mp3Asset.metadataForFormat(format) {
+                        if metadataItem.commonKey != nil {
+                            if metadataItem.commonKey == "artwork" {
+                                //picture
+                                let data = metadataItem.value as! NSData
+                                //self.imgView.image = UIImage(data: data)
+                                song.img = UIImage(data: data)
+                            } else if metadataItem.commonKey == "title" {
+                                //title
+                                let mp3Titile = metadataItem.value as! String
+                                song.title = mp3Titile
+                                
+                            } else if metadataItem.commonKey == "artist" {
+                                //artist
+                                song.artist = metadataItem.value as? String
+                            }
+                        }
+                    }
+                }
+                
+                self.sonLibary.append(song)
+            }
+        }
+    }
+    
+    func pause() {
+        if btnPause?.titleLabel?.text == "播放" {
+            btnPause?.setTitle("暂停", forState: .Normal)
+           // btnPause?.setTitleColor(UIColor.clearColor(), forState: .Normal)
+            btnPause?.setBackgroundImage(UIImage(named: "pause.png"), forState: .Normal)
+            player.play()
+        } else {
+            btnPause?.setTitle("播放", forState: .Normal)
+            //btnPause?.setTitleColor(UIColor.clearColor(), forState: .Normal)
+            btnPause?.setBackgroundImage(UIImage(named: "play.png"), forState: .Normal)
+            player.pause()
+        }
+        
+    }
+    
+    func stop() {
+        self.player.stop()
+        self.player.currentTime = 0
+        self.slider?.value = 0
+    }
+    
+    func progressCtr() {
+        //self.player.currentTime = self.player.duration * NSTimeInterval
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
